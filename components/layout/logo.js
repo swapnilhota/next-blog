dc70a671079cb5d19018f71384bc5ca9d1f4a1fd
Swapnil Hota <swapnilhota99@gmail.com>
2021-08-31 14:00:34 +0530

    update

---
+++ components/layout/logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './logo.module.css'
+
+const Logo = () => {
+    return (
+        <div className={classes.logo}>
+            Next Blog
+        </div>
+    )
+}
+
+export default Logo
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
+import classes from "./main-navigation.module.css"
+import Logo from './logo';
 
 const MainNavigation = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <Link href="/">
                 <a>
                     <Logo />
