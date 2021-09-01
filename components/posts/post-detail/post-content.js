cd590c9f15e2e6a3e986343aee7d700c9eddf79a
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-01 23:03:21 +0530

    update-post-content-comp

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostContent = () => {
+    return (
+        <article>
+            <PostHeader />
+            CONTENT
+        </article>
+    )
+}
+
+export default PostContent
