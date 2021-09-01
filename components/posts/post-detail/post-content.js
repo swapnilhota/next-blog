44e292c25a29eb813460b7b5c3dd057ef5f14386
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-01 23:10:53 +0530

    update-post-content-comp

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+const DUMMY_POST = {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    date: '2022-08-31',
+    content: '# This is a first post'
+}
+
 const PostContent = () => {
+
+    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
     return (
         <article>
-            <PostHeader />
-            CONTENT
+            <PostHeader title={DUMMY_POST.title} image={imagePath} />
+            {DUMMY_POST.content}
         </article>
     )
 }
