5c963ef402d9f960a9a90c6f7dfc4afc6b56c990
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-01 22:59:04 +0530

    update-all-posts-page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import AllPosts from '../../components/posts/all-posts'
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a React Framework for production',
+        date: '2022-08-31'
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a React Framework for production',
+        date: '2022-08-31'
+    },
+    {
+        slug: 'getting-started-with-nextjs3',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a React Framework for production',
+        date: '2022-08-31'
+    }
+]
 
 const AllPostsPage = () => {
     return (
-        <div>
-
-        </div>
+        <AllPosts posts={DUMMY_POSTS} />
     )
 }
 
