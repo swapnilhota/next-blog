017b457b14876cb56067ff8db34b3470e0d616c7
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-02 22:57:34 +0530

    update-contact-api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
     if (req.method === "POST") {
         const { email, name, message } = req.body;
 
@@ -14,7 +16,28 @@ const handler = (req, res) => {
             message
         };
 
-        console.log(newMessage);
+        let client;
+
+        try {
+            client = await MongoClient.connect('mongodb+srv://first-user:ErYzjK121br6I5gq@cluster0.t01a9.mongodb.net/NextBlog?retryWrites=true&w=majority');
+        } catch (error) {
+            res.status(500).json({ message: 'Could not connect to database' });
+            console.log(error);
+            return;
+        }
+
+        const db = client.db();
+
+        try {
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({ message: 'Storing message failed' });
+            return;
+        }
+
+        client.close();
 
         res.status(201).json({ message: 'Succesfully stored message', message: newMessage });
     }
