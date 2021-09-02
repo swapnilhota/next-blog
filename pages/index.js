2f1d2ee942fd607bdf730df46f6688c0d850c2bc
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-02 10:53:47 +0530

    update-home=page

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,25 @@
 import React, { Fragment } from 'react'
 import FeaturedPosts from '../components/home-page/featured-posts'
 import Hero from '../components/home-page/hero'
+import { getFeaturedPosts } from '../lib/posts-util'
 
-const DUMMY_POSTS = [
-    {
-        slug: 'getting-started-with-nextjs',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a React Framework for production',
-        date: '2022-08-31'
-    },
-    {
-        slug: 'getting-started-with-nextjs2',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a React Framework for production',
-        date: '2022-08-31'
-    },
-    {
-        slug: 'getting-started-with-nextjs3',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a React Framework for production',
-        date: '2022-08-31'
-    }
-]
-
-const HomePage = () => {
+const HomePage = (props) => {
     return (
         <Fragment>
             <Hero />
-            <FeaturedPosts posts={DUMMY_POSTS} />
+            <FeaturedPosts posts={props.posts} />
         </Fragment>
     )
+}
+
+export const getStaticProps = () => {
+    const featuredPosts = getFeaturedPosts();
+
+    return {
+        props: {
+            posts: featuredPosts
+        }
+    }
 }
 
 export default HomePage
