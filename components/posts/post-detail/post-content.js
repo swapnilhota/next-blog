c5ccff51837082ec800a206e8ea4c173df237c0c
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-02 17:57:41 +0530

    update-postContent-comp

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,22 +3,16 @@ import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
 
-const DUMMY_POST = {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    date: '2022-08-31',
-    content: '# This is a first post'
-}
+const PostContent = (props) => {
 
-const PostContent = () => {
+    const { post } = props;
 
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+    const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POST.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     )
 }
