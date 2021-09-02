18c9bb35c6e9dbca7b3e57bbceaf5106fd350b8a
Swapnil Hota <swapnilhota99@gmail.com>
2021-09-02 17:55:41 +0530

    update-postDetailPage

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,36 @@
 import React from 'react'
 import PostContent from '../../components/posts/post-detail/post-content'
+import { getPostData, getPostsFiles } from '../../lib/posts-util'
 
-const PostDetailPage = () => {
+const PostDetailPage = (props) => {
     return (
-        <PostContent />
+        <PostContent post={props.post} />
     )
+}
+
+export const getStaticProps = (context) => {
+    const { params } = context;
+    const { slug } = params;
+
+    const postData = getPostData(slug);
+
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600
+    }
+}
+
+export const getStaticPaths = () => {
+
+    const postFilenames = getPostsFiles();
+    const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ''));
+
+    return {
+        paths: slugs.map(slug => ({ params: { slug: slug } })),
+        fallback: false
+    }
 }
 
 export default PostDetailPage
