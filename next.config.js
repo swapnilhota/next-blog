ef50361d469a004dc1528ddd17d82c5a26320750
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-09-03 14:30:51 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    env: {
-        mongodb_connection_string: 'mongodb+srv://first-user:ErYzjK121br6I5gq@cluster0.t01a9.mongodb.net/NextBlog?retryWrites=true&w=majority'
-    }
-}
